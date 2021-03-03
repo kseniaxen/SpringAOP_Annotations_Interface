cb40d50d7385c08d5a9b4a3a33e4751bb5258e19
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-03-03 18:26:08 +0200

    xml->annotation

--- src/main/java/org/tyaa/spring/aop/main/Start.java
+++ src/main/java/org/tyaa/spring/aop/main/Start.java
@@ -1,6 +1,7 @@
 package org.tyaa.spring.aop.main;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.tyaa.spring.aop.objects.FileManager;
 import org.tyaa.spring.aop.objects.FileManager2;
@@ -8,7 +9,8 @@ import org.tyaa.spring.aop.objects.FileManager2;
 public class Start {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext("org.tyaa.spring.*");
 		FileManager fileManager = (FileManager) context.getBean("fileManager");
 		// FileManager2 fileManager2 = (FileManager2) context.getBean("fileManager2");
 		
@@ -17,6 +19,7 @@ public class Start {
 		//fileManager.getExtensionCount("c:\\Windows\\system32\\drivers");
 		//fileManager.getExtensionList("c:\\Windows\\system32\\drivers");
 		// fileManager2.getExtensionCount("c:\\Windows");
-		fileManager.getExtensionList("/home/yurii/Документы/книги");
+		fileManager.getExtensionList("c:\\\\Windows\\\\system32\\\\drivers");
+		//fileManager.getExtensionCount("c:\\\\Windows\\\\system32\\\\drivers");
 	}
 }
--- src/main/java/org/tyaa/spring/aop/objects/FileManager.java
+++ src/main/java/org/tyaa/spring/aop/objects/FileManager.java
@@ -7,42 +7,60 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 import org.tyaa.spring.aop.annotations.ShowResult;
 import org.tyaa.spring.aop.annotations.ShowTime;
 
 @Component
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class FileManager implements Manager {
 
 	@Override
 	@ShowTime
 	@ShowResult
 	public Set<String> getExtensionList(String folder) {
+		//создаем объект File для текущего каталога
 		File dir = new File(folder);
+		//создание коллекции String с неповторяющимися элементами и
+		//образует пустое дерево, которое будет сортировать элементы в порядке возрастания
 		Set<String> extList = new TreeSet<>();
+		//запускается цикл по файлам в заданной директории
 		for (File file : dir.listFiles()) {
+			//получаем название файла
 			String fileName = file.getName();
+			//вычисляем индекс символа в строке
+			// последнего вхождения точки в названии файла
 			int i = fileName.lastIndexOf(".");
+			//если это файл, а не каталог и индекс не равен -1
 			if (file.isFile() && i != -1) {
+				//в коллекцию добавляется подстрока с названия файла, начиная со следующего символа после точки
+				// и переходит в нижний регистр
 				extList.add(fileName.substring(i + 1).toLowerCase());
 			}
 		}
+		//возвращается полученная коллекция
 		return extList;
 	}
 
 	@Override
 	@ShowResult
 	public Map<String, Integer> getExtensionCount(String folder) {
-
+		//создаем объект File для текущего каталога
 		File dir = new File(folder);
-
+		//создаем коллекцию HashMap (ключ-значение) строка-целое, в котором ключ всегда является уникальным
 		Map<String, Integer> map = new HashMap<>();
 
+		//запускаем цикл по коллекции getExtensionList с неповторяющимися элементами
 		for (String ext : getExtensionList(folder)) {
+			//вызываем экземпляр реализованного интерфейса FilenameFilter (для фильтрации имен файлов в определенной папке)
+			// и передаем строку в коллекции
 			FilenameFilter filter = new CustomFileFilter(ext);
+			//в коллекцию добавляем ключ - название расширения файла,
+			//значение - количество файлов с определенным расширением в директории
 			map.put(ext, dir.listFiles(filter).length);
 		}
-
+		//возвращаем полученную коллекцию
 		return map;
 
 	}
